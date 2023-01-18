3f4f279054409ff0dd5cbe04b90e81cbfe15f92c
Yanawut Pomkaew <AnathamPK@gmail.com>
2023-01-19 00:06:59 +0700

    update code

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-let image = 1;
+let image = 0;
 display(image);
 
 function changeImage(n) {
@@ -10,34 +10,35 @@ function currentImage(n) {
 }
 
 function display(n) {
+    
     let slide = document.getElementsByClassName("slide");
     let dot = document.getElementsByClassName("dot");
-            
     // กรณีที่กด next ไปเรื่อย ๆ เมื่อถึงรูปสุดท้าย รูปที่แสดงต่อจากนี้จะเป็นรูปแรก
-    if (n > slide.length) {
-        image = 1
+   
+    if (n > slide.length-1) {
+        image = 0;
     }    
-
+    console.log(n);
     // กรณีที่กด previous ไปเรื่อย ๆ เมื่อถึงรูปแรก รูปที่แสดงต่อจากนี้จะเป็นรูปสุดท้าย
-    if (n < 1) {
-        image = slide.length
+    if (n < 0) {
+        image = slide.length-1;
     }
 
     for (let i = 0; i < slide.length; ++i) {
         // ถ้าไม่ใช่รูปที่เลื่อนดูอยู่ให้ซ่อนไว้
-        if(i+1!=image) {
+        if(i!=image) {
             slide[i].style.display = "none";  
         }
     }
 
     for (let i = 0; i < dot.length; ++i) {
         // ถ้าไม่ใช่เป็นจุดหน้าที่รูปแสดงผล ไม่ต้องแสดงสี
-        if(i+1!=image) {
+        if(i!=image) {
             dot[i].className = dot[i].className.replace(" active", "");      
         }
     }
             
     //รูปที่ดูอยู่ให้แสดงและจุดด้านล่างของหน้าที่แสดงต้องขึ้นเป็นสี
-    slide[image-1].style.display = "block";  
-    dot[image-1].className += " active";
+    slide[image].style.display = "block";  
+    dot[image].className += " active";
 }
